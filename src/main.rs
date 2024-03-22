20231d63e91918d69434997d7931143c326cc156
Peter Sideris <petersid2022@gmail.com>
2024-03-23 01:34:58 +0200

    feat: switched to clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use anyhow::Error;
+use clap::Parser;
 use rand::seq::SliceRandom;
 use reqwest::Client;
 use scraper::{Html, Selector};
 use std::time::Instant;
-use std::{env, process::exit, usize};
+use std::usize;
 use tabled::{
     settings::{
         object::Columns, style::Style, themes::ColumnNames, Alignment, Color, Modify, Width,
@@ -37,65 +38,25 @@ impl Function {
     }
 }
 
-fn help() {
-    println!(
-        "Usage:
-        -n [number]:    Set the number of products to print (default: 5)
-        -p [number]:    Set the number of pages to scrape (default: 1)"
-    );
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Set the number of products to print
+    #[arg(short, long = "number-of-items", default_value_t = 5)]
+    n: usize,
+
+    /// Set the number of pages to scrape
+    #[arg(short, long = "number-of-pages", default_value_t = 5)]
+    p: usize,
 }
 
-#[tokio::main(flavor = "multi_thread")]
+#[tokio::main]
 async fn main() {
+    let args = Args::parse();
     let t1 = Instant::now();
-    let args: Vec<String> = env::args().collect();
-    let mut number_of_items: usize = 5;
-    let mut number_of_pages: usize = 1;
-
-    if args.len() > 1 && args.len() != 3 {
-        help();
-        exit(0);
-    }
-
-    if args.len() == 3 {
-        match args[1].as_str() {
-            "-n" => match args[2].parse::<u32>() {
-                Ok(num) => {
-                    if num == 0 {
-                        help();
-                        exit(0);
-                    }
-                    number_of_items = num as usize;
-                }
-                _ => {
-                    println!("Invalid argument.");
-                    help();
-                    exit(0);
-                }
-            },
-            "-p" => match args[2].parse::<u32>() {
-                Ok(f) => {
-                    if f == 0 {
-                        help();
-                        exit(0);
-                    }
-                    number_of_pages = f as usize;
-                }
-                _ => {
-                    println!("Invalid argument.");
-                    help();
-                    exit(0);
-                }
-            },
-            _ => {
-                help();
-                exit(0);
-            }
-        }
-    }
 
     let client = reqwest::Client::new();
-    let data = scrape(&client, number_of_items, number_of_pages).await;
+    let data = scrape(&client, args.n, args.p).await;
     let mut table = match data {
         Ok(data) => Table::new(data),
         Err(err) => {
@@ -125,7 +86,7 @@ fn create_link(x: String, y: String) -> String {
     out
 }
 
-fn calculate_discount(original_price: &String, discounted_price: &String) -> Result<f32, Error> {
+fn calculate_discount(original_price: &str, discounted_price: &str) -> Result<f32, Error> {
     let original_price_str: String = original_price
         .chars()
         .filter(|c| char::is_numeric(*c) || *c == ',')
@@ -135,8 +96,8 @@ fn calculate_discount(original_price: &String, discounted_price: &String) -> Res
         .filter(|c| char::is_numeric(*c) || *c == ',')
         .collect();
 
-    let original_price_int: f32 = original_price_str.replace(",", ".").parse()?;
-    let discounted_price_int: f32 = discounted_price_str.replace(",", ".").parse()?;
+    let original_price_int: f32 = original_price_str.replace(',', ".").parse()?;
+    let discounted_price_int: f32 = discounted_price_str.replace(',', ".").parse()?;
 
     let discount = 100.0 * (original_price_int - discounted_price_int) / original_price_int;
 
@@ -148,28 +109,28 @@ async fn scrape(
     number_of_items: usize,
     number_of_pages: usize,
 ) -> Result<Vec<Function>, Error> {
-    let base_url = "https://www.skroutz.gr/price-drops?order_by=".to_string();
+    let base_url = "https://www.skroutz.gr/deals?order_by=".to_string();
     let url = format!("{}recommended&recent=1&page={}", base_url, number_of_pages);
 
     let response = client.get(url).send().await?;
 
     let body = response.text().await?;
     let fragment = Html::parse_fragment(&body);
-    let selector = Selector::parse(".sku-card.js-sku").unwrap();
+    let selector = Selector::parse(r#"li[class="cf card"]"#).unwrap();
     let mut data: Vec<Function> = Vec::new();
 
     for card in fragment.select(&selector) {
         let card_body = Html::parse_fragment(&card.html());
-        let selector = Selector::parse(".sku-card-info").unwrap();
+        let selector = Selector::parse(r#"div[class="card-content"]"#).unwrap();
         let products = card_body.select(&selector);
         for product in products {
             let product_body = Html::parse_fragment(&product.html());
 
-            let del_selector = Selector::parse("del").unwrap();
-            let a_selector = Selector::parse("p.sku-card-price a").unwrap();
+            let strike_selector = Selector::parse("strike").unwrap();
+            let a_selector = Selector::parse(r#"a[class="js-sku-link sku-link"]"#).unwrap();
 
             let original_price = product_body
-                .select(&del_selector)
+                .select(&strike_selector)
                 .next()
                 .map(|del| del.text().collect::<String>());
 
@@ -177,6 +138,12 @@ async fn scrape(
                 .select(&a_selector)
                 .next()
                 .map(|a| a.text().collect::<String>());
+
+            let discounted_price = discounted_price
+                .unwrap()
+                .chars()
+                .skip_while(|c| !char::is_numeric(*c))
+                .collect::<String>();
 
             let title = product_body
                 .select(&a_selector)
@@ -188,24 +155,25 @@ async fn scrape(
                 .next()
                 .map(|a| a.value().attr("href").unwrap_or_default());
 
-            if let (Some(title), Some(original_price), Some(discounted_price), Some(link)) =
+            if let (Some(title), Some(original_price), discounted_price, Some(link)) =
                 (title, original_price, discounted_price, link)
             {
                 let url = format!("https://skroutz.gr{}", link);
                 let link = create_link(url, "link".to_string());
                 let discount = calculate_discount(&original_price, &discounted_price)?;
                 data.push(Function::new(
-                    &title.trim().to_string(),
-                    &original_price.trim().to_string(),
-                    &discounted_price.trim().to_string(),
+                    title.trim(),
+                    original_price.trim(),
+                    discounted_price.trim(),
                     &discount.to_string(),
-                    &link.trim().to_string(),
+                    link.trim(),
                 ));
             }
         }
     }
 
-    data = data.drain(0..number_of_items).collect();
+    // Shuffle the data
+    data.truncate(number_of_items);
 
     let mut rng = rand::thread_rng();
     data.shuffle(&mut rng);
